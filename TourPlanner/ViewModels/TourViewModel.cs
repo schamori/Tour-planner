e80b90fe37e989b07480f9fb7de8b73e08eec531
schamori <21.5.2003.ms@gmail.com>
2024-03-27 18:03:16 +0100

    jk

--- TourPlanner/ViewModels/TourViewModel.cs
+++ TourPlanner/ViewModels/TourViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 namespace TourPlanner.ViewModels
 {
     public class TourViewModel: ViewModelBase
     {
+        public class Tour
+        {
+            public string Name { get; set; }
+        }
+
+        public ObservableCollection<Tour> Tours { get; set; }
+
+
+        public TourViewModel()
+        {
+            Tours = new ObservableCollection<Tour>
+        {
+            new Tour { Name = "Wienerwald" },
+            new Tour { Name = "Dopplerhütte" },
+            new Tour { Name = "FiglWarte" },
+            new Tour { Name = "Dorfrunde" }
+        };
+        }
     }
+
+    
+
 }
