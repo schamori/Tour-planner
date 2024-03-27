4d2e97bbbf6c0f513f5264ae70b084697d03dea2
schamori <21.5.2003.ms@gmail.com>
2024-03-27 15:54:06 +0100

    erf

---
+++ TourPlanner/ViewModels/AddTourViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.ViewModels
+{
+    // Properties bound to your input fields
+    public string TourName { get; set; }
+    public string TourDescription { get; set; }
+    // Other properties...
+
+    public AddTourViewModel()
+    {
+        SaveTourCommand = new RelayCommand(SaveTour);
+    }
+
+    private void SaveTour(object parameter)
+    {
+        // Logic to save the tour using the input data
+    }
+}
