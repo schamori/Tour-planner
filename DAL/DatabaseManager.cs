04e88afc16d02a22fe9587b2c19a9340772e21ba
artiicz <matthias2486@gmail.com>
2024-03-27 15:13:51 +0100

    dh

---
+++ DAL/DatabaseManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DAL
+{
+    public class DatabaseManager
+    {
+        private readonly string _connectionString;
+
+        public DatabaseManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void OpenConnection(Action<NpgsqlConnection> actionWithConnection)
+        {
+            using (var connection = new NpgsqlConnection(_connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    actionWithConnection(connection);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    // Handle exceptions (log them or show them to the user)
+                }
+            }
+        }
+    }
+}
---
+++ DAL/OpenRouteServiceClient.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace DAL
+{
+    public class OpenRouteServiceClient
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private readonly string _apiKey;
+
+        public OpenRouteServiceClient(string apiKey)
+        {
+            _apiKey = apiKey;
+        }
+
+        public async Task<JObject> GeocodeAddress(string address)
+        {
+            var response = await _client.GetAsync($"https://api.openrouteservice.org/geocode/search?api_key=5b3ce3597851110001cf62481e3cc9942506493089ff10a91977e5c0&text={Uri.EscapeDataString(address)}");
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(responseString);
+        }
+
+        public async Task<JObject> GetRoute(double startLat, double startLng, double endLat, double endLng)
+        {
+            var requestBody = new JObject(new JProperty("coordinates", new JArray(new JArray(startLng, startLat), new JArray(endLng, endLat))));
+            var content = new StringContent(requestBody.ToString(), Encoding.UTF8, "application/json");
+            var response = await _client.PostAsync($"https://api.openrouteservice.org/v2/directions/driving-car/geojson?api_key={_apiKey}", content);
+            response.EnsureSuccessStatusCode();
+            var responseString = await response.Content.ReadAsStringAsync();
+            return JObject.Parse(responseString);
+        }
+    }
+}
